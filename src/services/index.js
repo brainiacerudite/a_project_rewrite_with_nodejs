b0bf10f244b7fc84e9e2e4054d0cf51b204963b3
Brainiac Erudite <brainiachades@gmail.com>
2024-01-16 22:14:40 +0100

    Updated

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+const authService = require("./auth.service");
+const emailService = require("./email.service");
+const tokenService = require("./token.service");
+const userService = require("./user.service");
+
+module.exports = {
+  authService,
+  emailService,
+  tokenService,
+  userService,
+};
---
+++ src/services/user.service.js
@@ -0,0 +1,7 @@
+const getUserById = async (id) => {
+  //   return User.findById(id);
+};
+
+const getUserByEmail = async (email) => {
+  //   return User.findOne({email});
+};
